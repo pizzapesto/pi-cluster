1c73afe701f4ac0b33218cab229eee02c74d289f
NakkoWorld <marcmorlor@gmail.com>
2025-12-12 02:15:10 +0100

    Grab the API config

--- infra/vpn-stack/scripts/worker_init/wg_conf.sh
+++ infra/vpn-stack/scripts/worker_init/wg_conf.sh
@@ -23,7 +23,6 @@ if [[ "$ACCESS" == *'"authenticated":true'* ]]; then
             | tonumber' \
     | sort -n \
     )
-    echo $ordered_nums
     
     new_num=1
 
@@ -34,13 +33,26 @@ if [[ "$ACCESS" == *'"authenticated":true'* ]]; then
     done
 
     new_name="Worker-$new_num"
+
     echo "New Worker: $new_name"
 
-    curl -X POST "$API_URL/api/wireguard/client" \
+    curl -sS -X POST "$API_URL/api/wireguard/client" \
         -H "CF-Access-Client-Id: ${CF_ACCESS_CLIENT_ID}" \
         -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" \
         -H "Content-Type: application/json" \
-        -d "{\"name\":\"$new_name\"}"
+        -d "{\"name\":\"$new_name\"}" \
+        > /dev/null
+
+    names=$(curl -sS "$API_URL/api/wireguard/client" \
+        -H "CF-Access-Client-Id: ${CF_ACCESS_CLIENT_ID}" \
+        -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" )
+
+    worker_id=$(echo $names | jq -r --arg n "$new_name" '.[] | select(.name == $n) | .id')    
+    
+    config=$(curl -sS "$API_URL/api/wireguard/client/$worker_id/configuration" \
+        -H "CF-Access-Client-Id: ${CF_ACCESS_CLIENT_ID}" \
+        -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}")
+    echo "$config" | sed '/^DNS/d'
 
 else 
     echo "Not Authenticated. | Message: $ACCESS" 
