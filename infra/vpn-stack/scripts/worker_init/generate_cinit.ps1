a2780e62931a91007b46f7f42b2c67055e713cd6
NakkoWorld <marcmorlor@gmail.com>
2025-12-21 08:38:39 +0100

    Try-catch added in ps1 script

--- infra/vpn-stack/scripts/worker_init/generate_cinit.ps1
+++ infra/vpn-stack/scripts/worker_init/generate_cinit.ps1
@@ -1,7 +1,21 @@
-Get-Content .env.shell | ForEach-Object {
-  if ($_ -match '^\s*([^#][^=]+)=(.*)$') {
-    $env:$($matches[1].Trim()) = $matches[2].Trim()
-  }
-}
+$ErrorActionPreference = "Stop"
 
-python render.py worker-cloudinit.tpl.yaml user-data.yaml
+try {
+  Get-Content .env.shell | ForEach-Object {
+    if ($_ -match '^\s*([^#][^=]+)=(.*)$') {
+      $env:$($matches[1].Trim()) = $matches[2].Trim()
+    }
+  }
+
+  python render.py worker-cloudinit.tpl.yaml user-data.yaml
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "render.py failed with exit code $LASTEXITCODE"
+    }
+
+    Write-Host "✅ Script Successful"
+}
+catch {
+  Write-Error "❌ Execution failed: $_"
+  exit 1
+}
